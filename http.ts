58fbdcb2dc1249770b6e4c9f68b3e2eb924eed72
Sonny Lazuardi <sonnylazuardi@gmail.com>
2023-10-19 16:02:11 +0100

    Add express

--- http.ts
+++ http.ts
@@ -1,22 +1,26 @@
-
-const http = require("http").createServer(function(request, response) {
-  response.writeHead(200, {
-      'Content-Type': 'text/html'
-  });
-  response.write("Welcome Bun!");
-  response.end();
-
-});
-const io = require("socket.io")(http);
+import express from 'express'
+import { Server } from "socket.io"
+import path from 'path'
 const port = process.env.PORT || 3000;
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, "public")))
+
+const expressServer = app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
+
+const io = new Server(expressServer, {
+  cors: {
+      origin: '*'
+  }
+})
+
 io.on("connection", (socket) => {
     console.log(`User ${socket.id} connected`)
 
     socket.on('message', (data) => {
         io.emit('message', {user: socket.id.substring(0, 4), counter: data.counter })
     })
-});
-
-http.listen(port, () => {
-  console.log(`Socket.IO server running at ${port}`);
 });
